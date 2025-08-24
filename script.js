7792c21dba1c82700040b5cf62a87f72233168b9
Awais Ahmed <139741743+awaisahmad6059@users.noreply.github.com>
2025-08-24 17:43:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function openNewTab(link) {
+  window.open(link, '_blank');
+}
+
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+
+  // You can also store the user preference in localStorage for persistence
+  const isDarkMode = body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode);
+}
+
+// Check for user's preference on page load
+document.addEventListener("DOMContentLoaded", () => {
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+  if (isDarkMode) {
+    document.body.classList.add("dark-mode");
+  }
+});
